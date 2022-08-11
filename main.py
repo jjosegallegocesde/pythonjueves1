4e4425551dfcb3aa7e2b513f999e975195a120e3
308 <308@academicos.cesde.edu.co>
2022-08-11 08:29:39 -0500

    ntj2 se crea main con datos primitivos y entradas por teclado

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1. DATOS PRIMITIVOS
+nombre="Juan"
+edad=33
+estatura=1.62
+hinchaDelMejor=True
+comidaFavorita=None
+
+#2. DATOS QUE ENTRAN POR EL TECLADO
+numero1=int(input("digite un numero: "))
+numero2=int(input("digita numero 2: "))
+suma = numero1 + numero2
+print(f'la suma de {numero1} y {numero2} es {suma}')
